795c1d14f0a024ebe35908946452aa065d6594f3
Anna Beatriz <abc230400@gmail.com>
2026-03-23 12:00:13 -0300

    algo

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Parâmetro obrigatório
+Route::get('/produtos/{id}/{nome}/{preco}', function (int $id, string $nome, float $preco) {
+return response()->json(['id' => $id, 'nome' => $nome, 'preco' => $preco]);
+});
+
+
+
+// Parâmetro opcional (com valor padrão)
+Route::get('/categorias/{slug?}', function (string $slug = 'todas') {
+return response()->json(['categoria' => $slug]);
+});
+
+
+
+// Restrição: aceita apenas números inteiros
+Route::get('/produtos/{id}', function (int $id) {
+return response()->json(['produtos_id' => $id]);
+})->whereNumber('id');
+
+
+
+
+// Rota POST
+Route::post('/produtos', function () {
+return response()->json(['mensagem' => 'Produto criado com sucesso!'], statusCode: 201);
 });
