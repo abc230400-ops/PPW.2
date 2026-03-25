fb8b7164eadfb40e5672bcafe61f676277cb7d43
Anna Beatriz <abc230400@gmail.com>
2026-03-25 17:38:05 -0300

    exercicios

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,29 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Parâmetro obrigatório
-Route::get('/produtos/{id}/{nome}/{preco}', function (int $id, string $nome, float $preco) {
-return response()->json(['id' => $id, 'nome' => $nome, 'preco' => $preco]);
+Route::get('/produtos', function () {
+return [['id'=> 1, 'nome' => 'banana', 'preço' => 10],['id'=> 2, 'nome' => 'maça', 'preço' => 20],['id'=> 3, 'nome' => 'pera', 'preço' => 30]];
 });
 
-
-
-// Parâmetro opcional (com valor padrão)
-Route::get('/categorias/{slug?}', function (string $slug = 'todas') {
-return response()->json(['categoria' => $slug]);
-});
-
-
-
-// Restrição: aceita apenas números inteiros
 Route::get('/produtos/{id}', function (int $id) {
-return response()->json(['produtos_id' => $id]);
-})->whereNumber('id');
+return response()->json(['id' => $id, 'nome' => 'Produto ' . $id]);
+});
 
+Route::get('/produtos/{id}/avaliacao', function (int $id) {
+return response()->json(['id' => $id, 'nome' => 'Produto ' . $id, 'avaliacao' =>[['id' => 1, 'texto' => 'algo'],['id' => 2, 'texto' => 'algo 2']] ]);
+});
 
-
-
-// Rota POST
 Route::post('/produtos', function () {
-return response()->json(['mensagem' => 'Produto criado com sucesso!'], statusCode: 201);
+return response()->json(['mensagem' => 'Produto criado com sucesso'], 201);
+});
+
+Route::get('/', function(){
+return view('welcome');
+});
+
+Route::delete('/produtos/{id}', function ($id) {
+return response()->json(['mensagem' => 'Produto deletado com sucesso'], 200);
 });
